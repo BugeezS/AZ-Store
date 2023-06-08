d0c61a4501a1421907c8e7e912e5f04833b87564
Bruno <brunolb.lafont@gmail.com>
2023-06-08 12:01:27 +0200

    git add tailwind + img

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="output.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="flex flex-row">
+      <button> Button 1 </button>
+      <button> Button 2 </button>
+      <button> Button 3 </button>
+    </div>
+</body>
+</html>
