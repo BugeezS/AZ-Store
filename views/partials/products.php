ce2e4f0393aac88cbf9bff5151e00cb6832deb6c
LeShib <dibernardonikko@gmail.com>
2023-06-08 11:52:44 +0200

    img folder + products.php file

---
+++ views/partials/products.php
@@ -0,0 +1,16 @@
+<?php
+$items = [
+    [
+        'id' => 1,
+        'product' => 'Nike Air Max 270 blue',
+        'price' => 140,
+        'image_url' => '/img/shoe_one.png', 
+    ],
+    [
+        'id' => 2,
+        'product' => 'Nike Air Max 270 purple',
+        'price' => 140,
+        'image_url' => '/img/shoe_two.png', 
+    ],
+];
+?>
