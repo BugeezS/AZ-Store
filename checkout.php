f7c9e557c773c2ee1da405cd489735d1c53c6c79
LeShib <dibernardonikko@gmail.com>
2023-06-09 10:28:34 +0200

    form style finished

--- checkout.php
+++ checkout.php
@@ -8,49 +8,53 @@
         <script src="scripts/main.js"></script>
         <title>Checkout</title>
     </head>
-    <body>
-        <div class="w-full max-w-xs">
-            <form method="post" action="#" id="form">
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="firstName">First name :</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="firstName" type="text">
-                    <p class="text-red-500 text-xs italic">Please enter your first name.</p>
+    <body class="bg-gradient-to-b from-gray-800 to-gray-900">
+        <h1 class="text-2xl flex justify-center text-center mt-1 text-white">Checkout informations</h1>
+        <div class="flex justify-center text-center mt-1">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-lg" method="post" action="#" id="form">
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="firstName">First name :</label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="firstName" type="text" required>
+                    </div>
+                    <div class="w-full md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="lastName">Last name :</label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="lastName" type="text" required>
+                    </div>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="lastName">Last name :</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="lastName" type="text">
-                    <p class="text-red-500 text-xs italic">Please enter your last name.</p>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="mail">Mail :</label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="mail" type="email" required>
+                    </div>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="mail">Mail :</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="mail" type="email">
-                    <p class="text-red-500 text-xs italic">Please enter your email.</p>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="adress">Adress :</label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="adress" type="text" required>
+                    </div>
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="postCode">Post code :</label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="postCode" type="number">
+                    </div>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="adress">Adress :</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="adress" type="text">
-                    <p class="text-red-500 text-xs italic">Please enter your adress.</p>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="town">Town :</label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="town" type="text" required>
+                    </div>
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="country">Country :</label>
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="country" type="text" required>
+                    </div>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="postCode">Post code :</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="postCode" type="number">
-                    <p class="text-red-500 text-xs italic">Please enter your post code.</p>
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="town">Town :</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="town" type="text">
-                    <p class="text-red-500 text-xs italic">Please enter your town.</p>
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="country">Country :</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="country" type="text">
-                    <p class="text-red-500 text-xs italic">Please enter your country.</p>
-                <div class="flex items-center justify-between">
+                <div class="flex items-center justify-between flex justify-center">
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Submit</button>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="reset">Reset</button>
                 </div>
             </form>
         </div>
-
+        <div class="flex justify-center text-center text-white">
         <?php
             function names_validation(){
                 if ((isset($_POST['firstName']) AND isset($_POST['lastName'])) AND (!empty($_POST['firstName']) AND !empty($_POST['lastName']))){
@@ -70,7 +74,6 @@
                     return false;
                 }
             }
-
             function mail_validation(){
                 if (isset($_POST['mail']) AND !empty($_POST['mail'])){
                     $mail = filter_var($_POST['mail'], FILTER_SANITIZE_EMAIL);
@@ -88,7 +91,6 @@
                     return false;
                 }
             }
-            
             function adress_validation(){
                 if (((isset($_POST['adress']) AND isset($_POST['postCode'])) AND isset($_POST['town']) AND isset($_POST['country']))
                 AND (!empty($_POST['adress']) AND !empty($_POST['postCode']) AND !empty($_POST['town']) AND !empty($_POST['country']))){
@@ -115,14 +117,17 @@
                     return false;
                 }
             }
-            if(validation()){
-                // echo "<pre>";
-                // print_r($_POST);
-                // echo "</pre>";
-                echo "Thank you for your order !";
-            }else{
-                echo "the form does not pass validation !";
+            if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+                if(validation()){
+                    // echo "<pre>";
+                    // print_r($_POST);
+                    // echo "</pre>";
+                    echo "Thank you for your order ".$_POST['firstName']." !";
+                }else{
+                    echo "the form does not pass validation !";
+                }
             }
         ?>
+        </div>
     </body>
 </html>
