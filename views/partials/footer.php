1de80a406116cde40417481a806eab298836567b
Bruno <brunolb.lafont@gmail.com>
2023-06-09 14:03:13 +0200

    correct footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,8 +1,8 @@
-<footer class="text-white">
-        <ul class="flex flex-row justify-center">
-            <li class="pr-4">Home</li>
-            <li class="pr-4">About</li> 
-            <li class="pr-4">Product</li>
-            <li class="pr-4">Contact</li>
-        </ul>
-    </footer>
+<footer class="text-white -mt-6 mb-6">
+    <ul class="flex flex-row justify-center items-center">
+        <li class="pr-4">Home</li>
+        <li class="pr-4">About</li> 
+        <li class="pr-4">Product</li>
+        <li class="pr-4">Contact</li>
+    </ul>
+</footer>
