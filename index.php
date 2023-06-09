bb4916ab08939aad58e0d9e9688809465719bcf0
Bruno <brunolb.lafont@gmail.com>
2023-06-09 13:59:51 +0200

    add header and footer

--- index.php
+++ index.php
@@ -49,38 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Document</title>
 </head>
 <body class="bg-gradient-to-b from-gray-800 to-gray-900">
-    <header>
-        <nav class="flex flex-row justify-around  h-16 items-center">
-            <h2 class="text-white text-2xl font-bold">AZ-[store]</h2>
-            <ul class="flex flex-row ">
-                <li class="pr-4 text-white">Home</li>
-                <li class="pr-4 text-white">About</li>
-                <li class="pr-4 text-white">Products</li>
-                <li class="pr-4 text-white">Contact</li>
-            </ul>
-            <div class="flex flex-row">
-                <svg fill="#ffff" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-                    width="20px" height="20px" viewBox="0 0 902.86 902.86"
-                    xml:space="preserve" >
-                <g>
-                    <g>
-                        <path d="M671.504,577.829l110.485-432.609H902.86v-68H729.174L703.128,179.2L0,178.697l74.753,399.129h596.751V577.829z
-                            M685.766,247.188l-67.077,262.64H131.199L81.928,246.756L685.766,247.188z"/>
-                        <path d="M578.418,825.641c59.961,0,108.743-48.783,108.743-108.744s-48.782-108.742-108.743-108.742H168.717
-                            c-59.961,0-108.744,48.781-108.744,108.742s48.782,108.744,108.744,108.744c59.962,0,108.743-48.783,108.743-108.744
-                            c0-14.4-2.821-28.152-7.927-40.742h208.069c-5.107,12.59-7.928,26.342-7.928,40.742
-                            C469.675,776.858,518.457,825.641,578.418,825.641z M209.46,716.897c0,22.467-18.277,40.744-40.743,40.744
-                            c-22.466,0-40.744-18.277-40.744-40.744c0-22.465,18.277-40.742,40.744-40.742C191.183,676.155,209.46,694.432,209.46,716.897z
-                            M619.162,716.897c0,22.467-18.277,40.744-40.743,40.744s-40.743-18.277-40.743-40.744c0-22.465,18.277-40.742,40.743-40.742
-                            S619.162,694.432,619.162,716.897z"/>
-                    </g>
-                </g>
-                </svg>
-                <span class="text-white">Login</span>
-            </div>
-        </nav>
-        <hr class="border-1 border-gray-700">
-    </header>
+    <?php include './views/partials/header.php'; ?>
     <main>
         <section class="flex flex-row ">
             <div class ="w-2/5 pt-60 pl-24 mr-24">
@@ -118,7 +87,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </section>
 
 
-        <section class="text-white mb-64">
+        <section class="text-white mb-24">
             <div class="flex flex-col items-center">
                 <img src="./img/shoe_two.png" alt="shoes 2" class="w-96">
                 <div class="text-center w-1/4 ">
@@ -155,13 +124,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </main>
     <hr class="border-1 border-gray-700 mb-12">
 
-    <footer class="text-white">
-        <ul>
-            <li>Home</li>
-            <li>About</li> 
-            <li>Product</li>
-            <li>Contact</li>
-        </ul>
-    </footer>
+    <?php include './views/partials/footer.php'; ?>
+
 </body>
 </html>
---
+++ views/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="text-white">
+        <ul class="flex flex-row justify-center">
+            <li class="pr-4">Home</li>
+            <li class="pr-4">About</li> 
+            <li class="pr-4">Product</li>
+            <li class="pr-4">Contact</li>
+        </ul>
+    </footer>
